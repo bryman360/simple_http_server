253085fe17b401b793d34f5802e9157ea6af82c7
bryman360 <bryman360@yahoo.com>
2024-06-25 00:36:37 -0700

    Added threading to handle multiple requests

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import socket
+import threading
 
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    #server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    #server_socket.accept()[0].sendall(b"GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\"")
     print("Ready to receive")
-    conn, addr = server_socket.accept()
-    msg = conn.recv(2048).decode("utf-8")
+    while True:
+        conn, addr = server_socket.accept()
+        threading.Thread(target=send_response, args=(conn, addr)).start()
+        
+
+def send_response(conn, addr):
+    msg = conn.recv(1024).decode("utf-8")
     split_msg = msg.split()
     path = split_msg[1]
     reply=b"HTTP/1.1 404 Not Found\r\n\r\n"
@@ -20,14 +24,12 @@ def main():
         header_found = False
         for part in split_msg:
             if header_found:
-                agent_name_len = len(part)
                 reply = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str.encode(str(len(part))) + b"\r\n\r\n" + str.encode(part)
                 break
             if part == "User-Agent:":
                 header_found = True
 
-    print(reply)
-    conn.send(reply)
+    conn.sendall(reply)
 
 if __name__ == "__main__":
     main()
