95ebd968baffe7e0475ceb6abe39c41deccd21c3
bryman360 <bryman360@yahoo.com>
2024-06-25 21:18:56 -0700

    New Request class. Separated Response/Request

---
+++ Messages.py
@@ -0,0 +1,51 @@
+class Request:
+    def __init__(self, message: str) -> None:
+        self.request_type = None
+        self.http_opener = "HTTP/1.1"
+        self.path = ""
+        self.headers = {}
+        self.body = ""
+        self._parse_message(message)
+
+    def _parse_message(self, message: str):
+        # Split the Request line and the headers from the body
+        rn_parse = message.split('\r\n\r\n', 1)
+        request_line_and_headers = rn_parse[0].split('\r\n', 1)
+        self.body = rn_parse[1]
+
+        # Parse out the request line
+        request_line = request_line_and_headers[0].split()
+        self.request_type = request_line[0]
+        self.path = request_line[1]
+        self.http_opener = request_line[2]
+
+        # Parse out the headers
+        headers_and_values = request_line_and_headers[1].split('\r\n')
+        for header_and_value in headers_and_values:
+            header_and_value_split = header_and_value.split()
+            self.headers[header_and_value_split[0]] = header_and_value_split[1]
+
+    def __str__(self) -> str:
+        string = f"{self.request_type} {self.path} {self.http_opener}\r\n"
+        for header in self.headers:
+            string += f"{header}: {self.headers[header]}\r\n"
+        string += f"\r\n"
+        string += self.body
+        return string
+    
+
+class Response:
+    def __init__(self) -> None:
+        self.http_opener = "HTTP/1.1"
+        self.status_code = "404"
+        self.status = "Not Found"
+        self.headers = {}
+        self.body = ""
+
+    def __str__(self) -> str:
+        string = f"{self.http_opener} {self.status_code} {self.status}\r\n"
+        for header in self.headers:
+            string += f"{header}: {self.headers[header]}\r\n"
+        string += f"\r\n"
+        string += self.body
+        return string
--- main.py
+++ main.py
@@ -3,24 +3,12 @@ import os
 import socket
 import threading
 from typing import List
+from datetime import datetime
+
+from Messages import Request, Response
 
 
 file_storage_path = '/'
-
-
-class ResponseContent:
-    def __init__(self) -> None:
-        self.http_opener = "HTTP/1.1"
-        self.status_code = "404"
-        self.status = "Not Found"
-        self.headers = {}
-        self.body = ""
-
-    def __str__(self) -> str:
-        string = f"{{HTTP Opener: {self.http_opener}, Status Code: {self.status_code}, Status: {self.status}\n"
-        string += f"Headers: {self.headers}\n"
-        string += f"Body: '{self.body}' }}"
-        return string
 
 
 def main():
@@ -38,78 +26,73 @@ def main():
     server_addr = "localhost"
     server_port = 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    print(f"Starting HTTP server up at {server_addr} on port {server_port}...")
+    print(f"{datetime.now()} Starting HTTP server up at {server_addr} on port {server_port}...")
     while True:
-        print("Listening...")
         conn, addr = server_socket.accept()
         threading.Thread(target=handle_connection, args=(conn, addr)).start()
         
 
-def handle_connection(conn: socket, addr):
-    print("Established connection with", addr)
-    response_content = parse_message(conn)
-    send_response(conn, response_content)
+def handle_connection(conn: socket, addr) -> None:
+    print(datetime.now(), "Established connection with", addr, "and received a request")
+    response = receive_message(conn)
+    conn.sendall(str(response).encode())
+    print(datetime.now(), "Sent response message to", addr)
 
 
-def parse_message(conn: socket):
+def receive_message(conn: socket) -> Response:
     msg = conn.recv(1024).decode("utf-8")
-    split_msg = msg.split()
-    request_type = split_msg[0]
-    path = split_msg[1]
-    received_msg_content = split_msg[2:]
-
-    if request_type == "GET":
-        return gather_get_response_content(path, received_msg_content)
+    msg_content = Request(msg)
+    if msg_content.request_type == "GET":
+        return handle_get_request(msg_content)
+    elif msg_content.request_type == "POST":
+        return handle_post_request(msg_content)
     else:
-        conn.send(b"HTTP/1.1 400 Bad Request (Only GET supported)\r\n\r\n")
+        bad_response = Response()
+        bad_response.status_code = "400"
+        bad_response.status = "Bad Request"
+        return bad_response
 
 
-def gather_get_response_content(path: List[str], received_msg_content: List[str]):
-    response_content = ResponseContent()
-    if len(path) == 1:
-        response_content.status_code = "200"
-        response_content.status = "OK"
-        return response_content
+def handle_get_request(request: Request) -> Response:
+    response = Response()
+    if len(request.path) == 1:
+        response.status_code = "200"
+        response.status = "OK"
+        return response
     
-    split_path = path[1:].split('/')
+    split_path = request.path[1:].split('/')
     if split_path[0] == "echo":
-        response_content.status_code = "200"
-        response_content.status = "OK"
-        response_content.headers['Content-Type'] = 'text/plain'
-        response_content.headers['Content-Length'] = str(len(path[6:]))
-        response_content.body = path[6:]
+        response.status_code = "200"
+        response.status = "OK"
+        response.headers['Content-Type'] = 'text/plain'
+        response.headers['Content-Length'] = str(len(request.path[6:]))
+        response.body = request.path[6:]
     elif split_path[0] == "user-agent":
-        response_content.status_code = "200"
-        response_content.status = "OK"
-        response_content.headers['Content-Type'] = 'text/plain'
-        for i, content in enumerate(received_msg_content):
-            if content == "User-Agent:":
-                response_content.headers["Content-Length"] = str(len(received_msg_content[i + 1]))
-                response_content.body = received_msg_content[i + 1]
-                break
+        response.status_code = "200"
+        response.status = "OK"
+        response.headers['Content-Type'] = 'text/plain'
+        response.headers['Content-Length'] = str(len(request.headers['User-Agent']))
+        response.body = request.headers['User-Agent']
     elif split_path[0] == "files":
         for path_part in split_path[1:]:
             file_path = os.path.join(file_storage_path, path_part)
         try:
             with open(file_path) as file:
                 file_content = file.read()
-                response_content.status_code = "200"
-                response_content.status = "OK"
-                response_content.headers['Content-Type'] = 'application/octet-stream'
-                response_content.headers['Content-Length'] = str(len(file_content))
-                response_content.body = file_content
+                response.status_code = "200"
+                response.status = "OK"
+                response.headers['Content-Type'] = 'application/octet-stream'
+                response.headers['Content-Length'] = str(len(file_content))
+                response.body = file_content
         except:
             pass
         
-    return response_content
+    return response
 
 
-def send_response(conn: socket, response_content: ResponseContent)->bool:
-    reply = response_content.http_opener + " " + response_content.status_code + " " + response_content.status + "\r\n"
-    for header in response_content.headers:
-        reply += header + ": " + response_content.headers[header] + "\r\n"
-    reply += "\r\n" + response_content.body
-    conn.sendall(reply.encode())
+def handle_post_request(request: Request) -> Response:
+    response = Response()
+    return response
 
 
 if __name__ == "__main__":
@@ -117,4 +100,4 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print()
-        print("Shutting down HTTP server...")
+        print(datetime.now(), "Shutting down HTTP server...")
